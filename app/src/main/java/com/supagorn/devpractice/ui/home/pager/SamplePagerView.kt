3fdaa1299e591178b382f8f859a9909a22c30ddc
Bell <supagorn.pi@gmail.com>
2018-06-09 17:01:47 +0700

    Simple pager adapter -- easy to used.

--- app/src/main/java/com/supagorn/devpractice/ui/home/pager/SamplePagerView.kt
+++ app/src/main/java/com/supagorn/devpractice/ui/home/pager/SamplePagerView.kt
@@ -5,12 +5,16 @@ import android.os.Build
 import android.support.annotation.RequiresApi
 import android.util.AttributeSet
 import android.view.View
+import android.widget.ImageView
 import android.widget.LinearLayout
+import com.bumptech.glide.request.RequestOptions
 import com.supagorn.devpractice.R
 import com.supagorn.devpractice.customs.FragmentNavigation
+import com.supagorn.devpractice.customs.adapter.java.SimplePagerAdapter
 import com.supagorn.devpractice.model.SamplePagerEntity
-import com.supagorn.devpractice.ui.home.pager.adapter.SamplePagerAdapter
 import com.supagorn.devpractice.ui.home.pager.detail.SamplePagerDetailFragment
+import com.supagorn.devpractice.utils.GlideLoader
+import com.supagorn.devpractice.utils.ResolutionUtils
 import kotlinx.android.synthetic.main.view_sample_pager.view.*
 
 /**
@@ -47,20 +51,36 @@ class SamplePagerView : LinearLayout {
     }
 
     fun setSampleEntity(sampleEntities: ArrayList<SamplePagerEntity>) {
-        val pagerAdapter = SamplePagerAdapter()
-        pagerAdapter.setSamplePagerEntities(sampleEntities)
+        val pagerAdapter = SimplePagerAdapter<SamplePagerEntity>().setOnInflateViewListener(object : SimplePagerAdapter.OnInflateViewListener {
+            override fun getLayout(): Int {
+                return R.layout.layout_item_sample_pager
+            }
+
+            override fun <T> onBindViewHolder(item: T, itemView: View, position: Int) {
+                //cast object
+                item as SamplePagerEntity
+                val imgBanner = itemView.findViewById(R.id.imgBanner) as ImageView
+                //set banner height
+                imgBanner.layoutParams.height = ResolutionUtils.getBannerHeightFromRatio(this@SamplePagerView.context)
+                GlideLoader.load(this@SamplePagerView.context, item.imageUrl,
+                        RequestOptions().centerCrop(), imgBanner)
+            }
+        }).setOnItemClickListener(object : SimplePagerAdapter.OnItemClickListener {
+            override fun <T> onItemClicked(item: T) {
+                if (fragmentNavigation != null) {
+                    item as SamplePagerEntity
+                    fragmentNavigation!!.open(SamplePagerDetailFragment.newInstance(item))
+                }
+            }
+        })
+
+        pagerAdapter.setListItems(sampleEntities)
 
         //set product image list
         viewPager.adapter = pagerAdapter
         circleindicator.removeAllViews()
         if (pagerAdapter.count > 1) {
             circleindicator.setViewPager(viewPager)
-        }
-
-        pagerAdapter.setOnItemClickListener { samplePagerEntity ->
-            if (fragmentNavigation != null) {
-                fragmentNavigation!!.open(SamplePagerDetailFragment.newInstance(samplePagerEntity))
-            }
         }
     }
 
