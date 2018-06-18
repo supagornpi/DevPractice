b05a0f5651a84a2b626f779e2fc57bb864da1dd8
Bell <supagorn.pi@gmail.com>
2018-06-18 14:40:44 +0700

    bottom navigation view EX library. change color style.

--- app/src/main/java/com/supagorn/devpractice/MainActivity.kt
+++ app/src/main/java/com/supagorn/devpractice/MainActivity.kt
@@ -3,20 +3,19 @@ package com.supagorn.devpractice
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.internal.BottomNavigationItemView
-import android.support.design.internal.BottomNavigationMenuView
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v4.widget.DrawerLayout
-import android.util.Log
 import android.view.Gravity
 import android.view.MenuItem
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx
 import com.supagorn.devpractice.constants.AppEventsConstants
 import com.supagorn.devpractice.customs.BaseActivity
 import com.supagorn.devpractice.customs.FragmentStateManager
 import com.supagorn.devpractice.singleton.AppEventLogger
 import com.supagorn.devpractice.ui.sidebar.SideMenu
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : BaseActivity(), SideMenu {
 
@@ -73,9 +72,18 @@ class MainActivity : BaseActivity(), SideMenu {
             (fragmentStateManager as FragmentStateManager).changeFragment(0)
         }
 
-        removeShiftMode(navigation)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         navigation.setOnNavigationItemReselectedListener(mOnNavigationItemReselectedListener)
+
+        navigation.enableShiftingMode(false)
+        navigation.enableItemShiftingMode(false)
+        navigation.setTextVisibility(false)
+        // set icon size
+        val iconSize = 32f
+        navigation.setIconSize(iconSize, iconSize)
+        // set item height
+        navigation.itemHeight = BottomNavigationViewEx.dp2px(this, (iconSize + 16))
+
     }
 
     private fun getNavPositionFromMenuItem(menuItem: MenuItem): Int {
@@ -85,27 +93,6 @@ class MainActivity : BaseActivity(), SideMenu {
             R.id.navigation_library -> 2
             R.id.navigation_setting -> 3
             else -> -1
-        }
-    }
-
-    @SuppressLint("RestrictedApi")
-    private fun removeShiftMode(view: BottomNavigationView) {
-        val menuView = view.getChildAt(0) as BottomNavigationMenuView
-        try {
-            val shiftingMode = menuView.javaClass.getDeclaredField("mShiftingMode")
-            shiftingMode.isAccessible = true
-            shiftingMode.setBoolean(menuView, false)
-            shiftingMode.isAccessible = false
-            for (i in 0 until menuView.childCount) {
-                val item = menuView.getChildAt(i) as BottomNavigationItemView
-                item.setShiftingMode(false)
-                // set once again checked value, so view will be updated
-                item.setChecked(item.itemData.isChecked)
-            }
-        } catch (e: NoSuchFieldException) {
-            Log.e("ERROR NO SUCH FIELD", "Unable to get shift mode field")
-        } catch (e: IllegalAccessException) {
-            Log.e("ERROR ILLEGAL ALG", "Unable to change value of shift mode")
         }
     }
 
